b3781f70016e2b4716dcf4c0b3e5deac79abdbd8
himanshu5703 <97935988+himanshu5703@users.noreply.github.com>
2023-08-07 21:54:49 +0530

    Create zFunction.cpp

---
+++ String II/zFunction.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+
+        if(m == 0)
+           return 0;
+
+        if(m > n)
+           return -1;
+
+        for(int i=0; i<n-m+1; i++){
+            if(haystack.substr(i,m) == needle)
+                 return i;
+        }      
+
+        return -1;
+    }
+};
